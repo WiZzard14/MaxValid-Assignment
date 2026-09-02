544572cd98ed52605397e3f83aa4bdb358a7b9f0
WiZzard14 <mdridoy144169@gmail.com>
2026-09-03 03:22:05 +0600

    first commit

---
+++ maxvalid-blog-news/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <h1 className="text-3xl font-bold text-sky-600">
+        MaxValid Blog & News
+      </h1>
+    </div>
+  );
+}
+export default App;
