0c8139e2a704b68fee8664116ca58e94726bfbf7
WiZzard14 <mdridoy144169@gmail.com>
2026-09-04 02:35:13 +0600

    feat: refactor Firebase configuration to use environment variables and remove clear-db script

--- maxvalid-blog-news/src/utils/firebase.js
+++ maxvalid-blog-news/src/utils/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPasswor
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCADHiUv_wT7FnVOLfv2Iensl3y9Ojwl0A",
-  authDomain: "recipehub-142f4.firebaseapp.com",
-  projectId: "recipehub-142f4",
-  storageBucket: "recipehub-142f4.firebasestorage.app",
-  messagingSenderId: "530282459582",
-  appId: "1:530282459582:web:ff5658899213558a6789ef",
-  measurementId: "G-KMZXE1BHXE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
